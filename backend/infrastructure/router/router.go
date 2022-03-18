0fbfbaa3b301da60f18f65ee0c7815d83c782525
Marty <m.karkossa@ultraware.nl>
2022-03-18 12:43:11 +0100

    improve panichandler

--- backend/infrastructure/router/router.go
+++ backend/infrastructure/router/router.go
@@ -2,10 +2,13 @@ package router
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
+	"runtime"
+	"strings"
 
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 
 	"github.com/FallenTaters/streepjes/backend/application/auth"
 	"github.com/FallenTaters/streepjes/backend/application/order"
@@ -17,7 +20,7 @@ type Static func(filename string) ([]byte, error)
 func New(static Static, authService auth.Service, orderService order.Service) http.Handler {
 	r := echo.New()
 
-	r.Use(middleware.Recover())
+	r.Use(recoverMiddleware)
 	r.HTTPErrorHandler = func(err error, ctx echo.Context) {
 		r.DefaultHTTPErrorHandler(err, ctx)
 	}
@@ -35,6 +38,42 @@ func New(static Static, authService auth.Service, orderService order.Service) ht
 	publicRoutes(r, static, authService)
 
 	return r
+}
+
+func recoverMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		defer func() {
+			v := recover()
+			if v == nil {
+				return
+			}
+
+			fmt.Fprintf(os.Stderr, "(%s %s) panic: %s\n", c.Request().Method, c.Request().URL.Path, fmt.Sprint(v))
+			fmt.Fprintf(os.Stderr, getStack())
+			fmt.Println()
+
+			c.Error(nil)
+		}()
+		return next(c)
+	}
+}
+
+func getStack() string {
+	pc := make([]uintptr, 50)
+	entries := runtime.Callers(2, pc)
+	frames := runtime.CallersFrames(pc[:entries])
+
+	var stackTrace strings.Builder
+
+	for frame, more := frames.Next(); more; frame, more = frames.Next() {
+		if frame.Function == `runtime.gopanic` { // If a panic occurred, start at the frame that called panic
+			stackTrace.Reset()
+			continue
+		}
+
+		stackTrace.WriteString(fmt.Sprintf("%s:%d\n", frame.File, frame.Line))
+	}
+	return stackTrace.String()
 }
 
 func readJSON[T any](c echo.Context) (T, bool) {
