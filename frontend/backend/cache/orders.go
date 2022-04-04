fac45c039d191d9501de85fa12c6aa7dc8ddd7cf
PotatoesFall <markarkossa@gmail.com>
2022-04-04 19:21:30 +0200

    WIP history

---
+++ frontend/backend/cache/orders.go
@@ -0,0 +1,3 @@
+package cache
+
+// TODO: make a generic solution for this. Perhaps a type Cache[T] or so that takes an addFunc
---
+++ frontend/components/pages/history.go
@@ -0,0 +1,32 @@
+package pages
+
+import (
+	"github.com/FallenTaters/streepjes/domain/orderdomain"
+	"github.com/FallenTaters/streepjes/frontend/backend/cache"
+)
+
+type History struct {
+	Orders      []orderdomain.Order        `vugu:"data"`
+	MembersByID map[int]orderdomain.Member `vugu:"data"`
+}
+
+func (h *History) Init() {
+	go func() {
+		orders, err := cache.Orders()
+		if err != nil {
+			return
+		}
+		h.Orders = orders
+
+		members, err := cache.Members()
+		if err != nil {
+			return
+		}
+
+		membersByID := make(map[int]orderdomain.Member)
+		for _, member := range members {
+			membersByID[member.ID] = member
+		}
+		h.MembersByID = membersByID
+	}()
+}
